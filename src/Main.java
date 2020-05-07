ea742a78baa0bf2fc14b5206137267ff3254d046
sergei Shapovalov <senishapovalov@gmail.com>
2020-05-07 17:28:48 +0300

    end

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        float buying =13000.80f;
+        int mile =20;
+        float bonus = buying /mile;
+        System.out.println(bonus);
+    }
+}
